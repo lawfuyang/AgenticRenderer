ae1a4b9cb032d1661793ab07ced73674dad8afa4
Roger <53224523+lawfuyang@users.noreply.github.com>
2026-01-10 14:51:38 +0800

    feat: update Vulkan API version to 1.4 in CreateInstance

--- src/GraphicRHI.cpp
+++ src/GraphicRHI.cpp
@@ -238,7 +238,7 @@ bool GraphicRHI::CreateInstance()
     appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
     appInfo.pEngineName = "No Engine";
     appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
-    appInfo.apiVersion = VK_API_VERSION_1_3;
+    appInfo.apiVersion = VK_API_VERSION_1_4;
 
     vk::InstanceCreateInfo createInfo{};
     createInfo.pApplicationInfo = &appInfo;
