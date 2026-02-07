014182901df1f54993b26ee972ef6683272f7d8f
Roger <53224523+lawfuyang@users.noreply.github.com>
2026-02-07 11:16:12 +0800

    misc

--- src/TextureLoader.cpp
+++ src/TextureLoader.cpp
@@ -3,7 +3,6 @@
 #define STBI_ONLY_JPEG
 #define STBI_ONLY_PNG
 #define STB_IMAGE_IMPLEMENTATION
-#define STB_IMAGE_IMPLEMENTATION
 #include "../external/stb_image.h"
 
 class MemoryMappedDataReader : public ITextureDataReader
